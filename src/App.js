21a251d82c5d517c0100776d5f75a82a27b3436e
George Chepiga <egorchepiga@gmail.com>
2018-12-18 01:08:46 +0300

    deadline push (;

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import DataGrip from './containers/datagrip';
+import Auth from './containers/auth'
+import Navigation from './containers/navigation'
+
 
 class App extends Component {
 
@@ -8,11 +11,20 @@ class App extends Component {
         super(props);
     }
 
-
     render() {
         return (
           <div id="root">
-              <DataGrip />
+              <div className="container">
+                  <div className="row">
+                      <div className="col-md-2">
+                          <Auth />
+                      </div>
+                      <div className="col-md-10">
+                          { this.props.store.auth.auth && <Navigation />}
+                      </div>
+                  </div>
+              </div>
+              <DataGrip data={this.props.store.navigation.dbData}/>
           </div>
         );
     };
